a028c6105a7727c1a3e5ae735435e2fa72c4a3bb
Umang Raghuvanshi <u@umangis.me>
2017-06-14 01:14:05 +0530

    Remove manual declarations for WINAPI functions

--- Prometheus/MemoryManager.h
+++ Prometheus/MemoryManager.h
@@ -3,11 +3,6 @@
 #include <TlHelp32.h>
 #include <vector>
 
-// I don't know why but VS doesn't like me
-HANDLE WINAPI CreateToolhelp32Snapshot(DWORD, DWORD);
-BOOL WINAPI Process32FirstW(HANDLE, LPPROCESSENTRY32);
-BOOL WINAPI Process32NextW(HANDLE, LPPROCESSENTRY32);
-
 class MemoryManager
 {
 public:
