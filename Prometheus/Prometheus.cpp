eeb555b8c767d5f2325f53770bf9961c5141d74a
Umang Raghuvanshi <u@umangis.me>
2017-06-15 11:12:36 +0530

    Allow restarting the hack threads

--- Prometheus/Prometheus.cpp
+++ Prometheus/Prometheus.cpp
@@ -17,6 +17,7 @@ void ShowUsage() {
 	std::cout << "Usage:" << std::endl;
 	std::cout << "Toggle Bunny Hop - <F6>" << std::endl;
 	std::cout << "Toggle Radar Hack - <F7>" << std::endl;
+	std::cout << "Restart all threads - <F11>" << std::endl;
 	std::cout << "Quit - <F12>" << std::endl;
 }
 
@@ -45,8 +46,18 @@ int main()
 				std::cout << "Enabling Radar Hack..." << std::endl;
 			RadarHackControl = !RadarHackControl;
 		}
+		else if (GetAsyncKeyState(VK_F11)) {
+			TerminateThread(hBHopThread, 0);
+			TerminateThread(hRadarThread, 0);
+			BHopControl = 0;
+			RadarHackControl = 0;
+			hBHopThread = CreateThread(NULL, NULL, BHopStart, &BHopControl, NULL, NULL);
+			hRadarThread = CreateThread(NULL, NULL, StartRadar, &RadarHackControl, NULL, NULL);
+			std::cout << "Restarted all threads..." << std::endl;
+		}
 		else if (GetAsyncKeyState(VK_F12)) {
 			TerminateThread(hBHopThread, 0);
+			TerminateThread(hRadarThread, 0);
 			exit(0);
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
