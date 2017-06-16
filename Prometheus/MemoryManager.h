a2cdc85417753d1f49c65fd4049438da98102b26
Umang Raghuvanshi <u@umangis.me>
2017-06-16 16:33:37 +0530

    MemoryManager: Don't panic on RW failures

--- Prometheus/MemoryManager.h
+++ Prometheus/MemoryManager.h
@@ -18,14 +18,12 @@ public:
 
 	template <class T> inline void Read(DWORD dwAddr, T& Value) {
 		if (!ReadProcessMemory(m_hProcess, (LPVOID)dwAddr, reinterpret_cast<LPVOID>(&Value), sizeof(T), NULL)) {
-			printf("Address: 0x%x\n", dwAddr);
-			Utils::ErrorAndExit("Couldn't read memory!");
+			printf("Address: 0x%x failed reading!\n", dwAddr);
 		}
 	}
 	template <class T> inline void Write(DWORD dwAddr, T& Value) {
 		if (!WriteProcessMemory(m_hProcess, (LPVOID)dwAddr, reinterpret_cast<LPVOID>(&Value), sizeof(T), NULL)) {
-			printf("Address: 0x%x\n", dwAddr);
-			Utils::ErrorAndExit("Couldn't write memory!");
+			printf("Address: 0x%x failed writing!\n", dwAddr);
 		}
 	}
 
